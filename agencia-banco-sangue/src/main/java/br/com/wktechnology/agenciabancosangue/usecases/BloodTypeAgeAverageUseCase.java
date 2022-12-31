9e12d31f4e75e7834a198448bc5e864fd85d3fce
pedrocampos <pedro.campos@startaideia.com.br>
2022-12-31 15:55:09 -0300

    fix: refatorando a classe BloodTypeAgeAverageUseCase.java

--- agencia-banco-sangue/src/main/java/br/com/wktechnology/agenciabancosangue/usecases/BloodTypeAgeAverageUseCase.java
+++ agencia-banco-sangue/src/main/java/br/com/wktechnology/agenciabancosangue/usecases/BloodTypeAgeAverageUseCase.java
@@ -19,54 +19,35 @@ public class BloodTypeAgeAverageUseCase {
 
     public List<BloodTypeAgeAverageResponseJson> get() {
         // dividir em listas por tipo sanguineo
-        List<Person> bloodTypeAPlus = this.getPersonByBloodType.get(BloodType.A_PLUS.getName());
-        int aPlus = (int) bloodTypeAPlus.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
-        BloodTypeAgeAverageResponseJson aPlusBloodTypeAgeAverageResponseJson = new BloodTypeAgeAverageResponseJson();
-        aPlusBloodTypeAgeAverageResponseJson.setBloodType(BloodType.A_PLUS.getDescription());
-        aPlusBloodTypeAgeAverageResponseJson.setAverage(aPlus);
+        BloodTypeAgeAverageResponseJson aPlusBloodTypeAgeAverageResponseJson = getaPlusBloodTypeAgeAverageResponseJson();
+        BloodTypeAgeAverageResponseJson aNegativeBloodTypeAgeAverageResponseJson = getaNegativeBloodTypeAgeAverageResponseJson();
+        BloodTypeAgeAverageResponseJson bPlusBloodTypeAgeAverageResponseJson = getbPlusBloodTypeAgeAverageResponseJson();
+        BloodTypeAgeAverageResponseJson bNegativeBloodTypeAgeAverageResponseJson = getbNegativeBloodTypeAgeAverageResponseJson();
+        BloodTypeAgeAverageResponseJson oPlusBloodTypeAgeAverageResponseJson = getoPlusBloodTypeAgeAverageResponseJson();
+        BloodTypeAgeAverageResponseJson oNegativeBloodTypeAgeAverageResponseJson = getoNegativeBloodTypeAgeAverageResponseJson();
+        BloodTypeAgeAverageResponseJson abPlusBloodTypeAgeAverageResponseJson = getAbPlusBloodTypeAgeAverageResponseJson();
+        BloodTypeAgeAverageResponseJson abNegativeBloodTypeAgeAverageResponseJson = getAbNegativeBloodTypeAgeAverageResponseJson();
 
-        List<Person> bloodTypeANegative = this.getPersonByBloodType.get(BloodType.A_NEGATIVE.getName());
-        int aNegative = (int) bloodTypeANegative.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
-        BloodTypeAgeAverageResponseJson aNegativeBloodTypeAgeAverageResponseJson = new BloodTypeAgeAverageResponseJson();
-        aNegativeBloodTypeAgeAverageResponseJson.setBloodType(BloodType.A_NEGATIVE.getDescription());
-        aNegativeBloodTypeAgeAverageResponseJson.setAverage(aNegative);
+        return getResponseJson(
+                aPlusBloodTypeAgeAverageResponseJson,
+                aNegativeBloodTypeAgeAverageResponseJson,
+                bPlusBloodTypeAgeAverageResponseJson,
+                bNegativeBloodTypeAgeAverageResponseJson,
+                oPlusBloodTypeAgeAverageResponseJson,
+                oNegativeBloodTypeAgeAverageResponseJson,
+                abPlusBloodTypeAgeAverageResponseJson,
+                abNegativeBloodTypeAgeAverageResponseJson);
+    }
 
-        List<Person> bloodTypeBPlus = this.getPersonByBloodType.get(BloodType.B_PLUS.getName());
-        int bPlus = (int) bloodTypeBPlus.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
-        BloodTypeAgeAverageResponseJson bPlusBloodTypeAgeAverageResponseJson = new BloodTypeAgeAverageResponseJson();
-        bPlusBloodTypeAgeAverageResponseJson.setBloodType(BloodType.B_PLUS.getDescription());
-        bPlusBloodTypeAgeAverageResponseJson.setAverage(bPlus);
-
-        List<Person> bloodTypeBNegative = this.getPersonByBloodType.get(BloodType.B_NEGATIVE.getName());
-        int bNegative = (int) bloodTypeBNegative.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
-        BloodTypeAgeAverageResponseJson bNegativeBloodTypeAgeAverageResponseJson = new BloodTypeAgeAverageResponseJson();
-        bNegativeBloodTypeAgeAverageResponseJson.setBloodType(BloodType.B_NEGATIVE.getDescription());
-        bNegativeBloodTypeAgeAverageResponseJson.setAverage(bNegative);
-
-        List<Person> bloodTypeOPlus = this.getPersonByBloodType.get(BloodType.O_PLUS.getName());
-        int oPlus = (int) bloodTypeOPlus.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
-        BloodTypeAgeAverageResponseJson oPlusBloodTypeAgeAverageResponseJson = new BloodTypeAgeAverageResponseJson();
-        oPlusBloodTypeAgeAverageResponseJson.setBloodType(BloodType.O_PLUS.getDescription());
-        oPlusBloodTypeAgeAverageResponseJson.setAverage(oPlus);
-
-        List<Person> bloodTypeONegative = this.getPersonByBloodType.get(BloodType.O_NEGATIVE.getName());
-        int oNegative = (int) bloodTypeONegative.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
-        BloodTypeAgeAverageResponseJson oNegativeBloodTypeAgeAverageResponseJson = new BloodTypeAgeAverageResponseJson();
-        oNegativeBloodTypeAgeAverageResponseJson.setBloodType(BloodType.O_NEGATIVE.getDescription());
-        oNegativeBloodTypeAgeAverageResponseJson.setAverage(oNegative);
-
-        List<Person> bloodTypeABPlus = this.getPersonByBloodType.get(BloodType.AB_PLUS.getName());
-        int abPlus = (int) bloodTypeABPlus.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
-        BloodTypeAgeAverageResponseJson abPlusBloodTypeAgeAverageResponseJson = new BloodTypeAgeAverageResponseJson();
-        abPlusBloodTypeAgeAverageResponseJson.setBloodType(BloodType.AB_PLUS.getDescription());
-        abPlusBloodTypeAgeAverageResponseJson.setAverage(abPlus);
-
-        List<Person> bloodTypeABNegative = this.getPersonByBloodType.get(BloodType.AB_NEGATIVE.getName());
-        int abNegative = (int) bloodTypeABNegative.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
-        BloodTypeAgeAverageResponseJson abNegativeBloodTypeAgeAverageResponseJson = new BloodTypeAgeAverageResponseJson();
-        abNegativeBloodTypeAgeAverageResponseJson.setBloodType(BloodType.AB_NEGATIVE.getDescription());
-        abNegativeBloodTypeAgeAverageResponseJson.setAverage(abNegative);
-
+    private List<BloodTypeAgeAverageResponseJson> getResponseJson(
+            final BloodTypeAgeAverageResponseJson aPlusBloodTypeAgeAverageResponseJson,
+            final BloodTypeAgeAverageResponseJson aNegativeBloodTypeAgeAverageResponseJson,
+            final BloodTypeAgeAverageResponseJson bPlusBloodTypeAgeAverageResponseJson,
+            final BloodTypeAgeAverageResponseJson bNegativeBloodTypeAgeAverageResponseJson,
+            final BloodTypeAgeAverageResponseJson oPlusBloodTypeAgeAverageResponseJson,
+            final BloodTypeAgeAverageResponseJson oNegativeBloodTypeAgeAverageResponseJson,
+            final BloodTypeAgeAverageResponseJson abPlusBloodTypeAgeAverageResponseJson,
+            final BloodTypeAgeAverageResponseJson abNegativeBloodTypeAgeAverageResponseJson) {
         List<BloodTypeAgeAverageResponseJson> averageResponseJsonList = new ArrayList<>();
         averageResponseJsonList.add(aPlusBloodTypeAgeAverageResponseJson);
         averageResponseJsonList.add(aNegativeBloodTypeAgeAverageResponseJson);
@@ -76,7 +57,61 @@ public class BloodTypeAgeAverageUseCase {
         averageResponseJsonList.add(oNegativeBloodTypeAgeAverageResponseJson);
         averageResponseJsonList.add(abPlusBloodTypeAgeAverageResponseJson);
         averageResponseJsonList.add(abNegativeBloodTypeAgeAverageResponseJson);
-
         return averageResponseJsonList;
+    }
+
+    private BloodTypeAgeAverageResponseJson getAbNegativeBloodTypeAgeAverageResponseJson() {
+        List<Person> bloodTypeABNegative = this.getPersonByBloodType.get(BloodType.AB_NEGATIVE.getName());
+        int abNegative = (int) bloodTypeABNegative.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
+        return getBloodTypeAgeAverageResponseJson(abNegative, BloodType.AB_NEGATIVE.getDescription());
+    }
+
+    private BloodTypeAgeAverageResponseJson getAbPlusBloodTypeAgeAverageResponseJson() {
+        List<Person> bloodTypeABPlus = this.getPersonByBloodType.get(BloodType.AB_PLUS.getName());
+        int abPlus = (int) bloodTypeABPlus.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
+        return getBloodTypeAgeAverageResponseJson(abPlus, BloodType.AB_PLUS.getDescription());
+    }
+
+    private BloodTypeAgeAverageResponseJson getoNegativeBloodTypeAgeAverageResponseJson() {
+        List<Person> bloodTypeONegative = this.getPersonByBloodType.get(BloodType.O_NEGATIVE.getName());
+        int oNegative = (int) bloodTypeONegative.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
+        return getBloodTypeAgeAverageResponseJson(oNegative, BloodType.O_NEGATIVE.getDescription());
+    }
+
+    private BloodTypeAgeAverageResponseJson getoPlusBloodTypeAgeAverageResponseJson() {
+        List<Person> bloodTypeOPlus = this.getPersonByBloodType.get(BloodType.O_PLUS.getName());
+        int oPlus = (int) bloodTypeOPlus.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
+        return getBloodTypeAgeAverageResponseJson(oPlus, BloodType.O_PLUS.getDescription());
+    }
+
+    private BloodTypeAgeAverageResponseJson getbNegativeBloodTypeAgeAverageResponseJson() {
+        List<Person> bloodTypeBNegative = this.getPersonByBloodType.get(BloodType.B_NEGATIVE.getName());
+        int bNegative = (int) bloodTypeBNegative.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
+        return getBloodTypeAgeAverageResponseJson(bNegative, BloodType.B_NEGATIVE.getDescription());
+    }
+
+    private BloodTypeAgeAverageResponseJson getbPlusBloodTypeAgeAverageResponseJson() {
+        List<Person> bloodTypeBPlus = this.getPersonByBloodType.get(BloodType.B_PLUS.getName());
+        int bPlus = (int) bloodTypeBPlus.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
+        return getBloodTypeAgeAverageResponseJson(bPlus, BloodType.B_PLUS.getDescription());
+    }
+
+    private BloodTypeAgeAverageResponseJson getaNegativeBloodTypeAgeAverageResponseJson() {
+        List<Person> bloodTypeANegative = this.getPersonByBloodType.get(BloodType.A_NEGATIVE.getName());
+        int aNegative = (int) bloodTypeANegative.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
+        return getBloodTypeAgeAverageResponseJson(aNegative, BloodType.A_NEGATIVE.getDescription());
+    }
+
+    private BloodTypeAgeAverageResponseJson getaPlusBloodTypeAgeAverageResponseJson() {
+        List<Person> bloodTypeAPlus = this.getPersonByBloodType.get(BloodType.A_PLUS.getName());
+        int aPlus = (int) bloodTypeAPlus.stream().mapToInt(Person::getAge).average().orElse(Double.NaN);
+        return getBloodTypeAgeAverageResponseJson(aPlus, BloodType.A_PLUS.getDescription());
+    }
+
+    private BloodTypeAgeAverageResponseJson getBloodTypeAgeAverageResponseJson(final int aPlus, final String description) {
+        BloodTypeAgeAverageResponseJson aPlusBloodTypeAgeAverageResponseJson = new BloodTypeAgeAverageResponseJson();
+        aPlusBloodTypeAgeAverageResponseJson.setBloodType(description);
+        aPlusBloodTypeAgeAverageResponseJson.setAverage(aPlus);
+        return aPlusBloodTypeAgeAverageResponseJson;
     }
 }
